506448a94f3166a33bec3519423d9b5943d4b187
Vasudevsharma666 <vasudev.sharma@gemsessence.com>
2020-12-01 19:56:26 +0530

    it

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -6,24 +6,18 @@ import startPage from './src/components/startpage';
 import Login from './src/components/login';
 import signIn from './src/components/signin';
 import DrawerNavigator from './src/utils/nativation/drawerNavigatoion';
-import {useSelector} from 'react-redux';
+
 const Stack = createStackNavigator();
 
-const App = () => {
-  const token = useSelector((state) => state.login);
-  useEffect(() => {
-    console.log(token.tokenData);
-  }, [token]);
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" headerMode={null}>
-        <Stack.Screen name="Home" component={startPage} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignIn" component={signIn} />
-        <Stack.Screen name="Drawer" component={DrawerNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home" headerMode={null}>
+      <Stack.Screen name="Home" component={startPage} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignIn" component={signIn} />
+      <Stack.Screen name="Drawer" component={DrawerNavigator} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
